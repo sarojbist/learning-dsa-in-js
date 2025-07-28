3eabaf5172e65c44e8c81eeb17021e6ddc5254c9
Saroj Bist Personal <bistsaroj24@gmail.com>
2025-07-29 00:24:31 +0530

    add sum of n numbers

---
+++ practice/sorts/bubbleSort.js
@@ -0,0 +1,21 @@
+let arr = [4, 2, 1, 3, 8, 9, -7];
+
+var bubbleSort = function (arr) {
+    let len = arr.length;
+
+    for (let i = 0; i < len - 1; i++) {
+        for (let j = 0; j < len - 1 - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+
+    }
+    return arr;
+};
+
+const sorted = bubbleSort(arr)
+
+console.log(sorted)
